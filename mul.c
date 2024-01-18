82ee864d167339320e2825a878908651a1965c1b
Mahari9 <merytseg77@gmail.com>
2024-01-18 03:32:44 -0800

    Implement the mul opcode

--- mul.c
+++ mul.c
@@ -3,7 +3,7 @@
 /**
  * mul - Multiplies the second top element of the stack
  * with the top element of the stack
- * @top: Pointer to the top of stack
+ * @top: Double pointer to the top of stack
  * @lin_num: Number of line
  */
 void mul(stack_t **top, unsigned int lin_num)
